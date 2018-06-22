a41f77f0654bb1cdf213b115b4e76a1562525c70
Bianca Jemsten <biancajemsten@gmail.com>
2018-06-22 12:24:40 +0100

    set up server

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const {port, dbURI} = require('./config/environment');
-
 const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 mongoose.connect(dbURI);
 
+const routes = require('./config/routes');
+
+
+app.use(bodyParser.json());
+app.use('/api', routes);
 
 
 app.listen(port, () => console.log(`Cruisin' into port ${port}`));
+
+module.exports = app;
