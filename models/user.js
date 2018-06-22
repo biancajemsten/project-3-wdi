6c0133aa18c2e27116e1ab71f868149bf386d6e2
dcravenor <happy-james@hotmail.co.uk>
2018-06-22 12:12:53 +0100

    added user Virtual

--- models/user.js
+++ models/user.js
@@ -4,11 +4,21 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   firstname: {type: String , required: true },
   lastname: {type: String , required: true},
-  email: {type: String , required: true},
+  email: {type: String , required: true, unique: true},
   password: {type: String , required: true} ,
   location: {type: String , required: true},
-  musicgenres: {type: String , required: true}
+  musicgenres: {type: String , required: false}
 
 });
+
+userSchema.virtual('bundles', {
+  localField: '_id',
+  foreignField: 'creator',
+  ref: 'Bundle'
+});
+
+// user virtual for password passwordConfirmation
+
+// user virtual for
 
 module.exports = mongoose.model('User', userSchema);
