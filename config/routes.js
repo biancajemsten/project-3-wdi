8efa5857a11214fd458121641ca18d8bfcbae655
Bianca Jemsten <biancajemsten@gmail.com>
2018-06-22 15:29:26 +0100

    added login route and controller and tested in insomnia

--- config/routes.js
+++ config/routes.js
@@ -6,5 +6,6 @@ const auth = require('../controllers/auth');
 router.get('/events', skiddle.eventFinder);
 
 router.post('/register', auth.register);
+router.post('/login', auth.login);
 
 module.exports = router;
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
-// const jwt = require('jsonwebtoken');
-// const {secret} = require('../config/environment');
+const jwt = require('jsonwebtoken');
+const {secret} = require('../config/environment');
 
 function register(req, res, next){
   User.create(req.body)
@@ -8,6 +8,23 @@ function register(req, res, next){
     .catch(next);
 }
 
+function login(req, res, next){
+  User.findOne({email: req.body.email})
+    .then(user => {
+      if(!user || !user.validatePassword(req.body.password)){
+        return res.status(401).json({message: 'Unauthorized'});
+      }
+      const token = jwt.sign({ sub: user._id}, secret, {expiresIn: '8h'});
+
+      res.json({
+        user,
+        token,
+        message: `Welcome back ${user.firstname} ${user.lastname}`});
+    })
+    .catch(next);
+}
+
 module.exports = {
-  register
+  register,
+  login
 };
