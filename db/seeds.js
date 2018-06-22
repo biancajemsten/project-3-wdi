ae03b28bc9ecbb87a0aacc44cfebd1b66395c834
Bella Dear <arabelladear@hotmail.co.uk>
2018-06-22 18:34:12 +0100

    two bundle seeds completed and embedded in users

--- db/seeds.js
+++ db/seeds.js
@@ -34,58 +34,60 @@ mongoose.connect(dbURI, (err, db) => {
     location: 'London',
     musicgenres: 'Metal'
   }])
-    .then(users => console.log(`${users.length} User(s) created`))
+    .then(users => {
+      console.log(`${users.length} User(s) created`);
+      return Bundle.create([{
+        event: {
+          name: 'Diamonds Are Forever',
+          date: 'Friday 20th July 2018',
+          location: {
+            lat: 53.4767,
+            lng: 2.2390
+          }
+        },
+        bar: {
+          name: 'The Alchemist',
+          location: {
+            lat: 53.4358,
+            lng: 2.9576
+          }
+        },
+        restaurant: {
+          name: 'The Grill on New York Street',
+          location: {
+            lat: 53.2922,
+            lng: 2.14407
+          }
+        },
+        creator: users[0]
+
+      }, {
+        event: {
+          name: 'Gentlemen\'s Dub Club',
+          date: 'Friday 19th October 2018',
+          location: {
+            lat: 51.4602,
+            lng: 0.1169
+          }
+        },
+        bar: {
+          name: 'The Blues Kitchen',
+          location: {
+            lat: 51.460313,
+            lng: -0.120439
+          }
+        },
+        restaurant: {
+          name: 'Trinity Arms',
+          location: {
+            lat: 51.4619,
+            lng: -0.1188
+          }
+        },
+        creator: users[1]
+      }]);
+    })
+    .then(bundles => console.log(`${bundles.length} Bundle(s) created`))
     .catch(err => console.log(err))
     .finally(() =>  mongoose.connection.close());
 });
-
-Bundle.create([{
-  event: {
-    name: 'Diamonds Are Forever',
-    date: 'Friday 20th July 2018',
-    location: {
-      lat: 53.4767,
-      lng: 2.2390
-    }
-  },
-  bar: {
-    name: 'The Alchemist',
-    location: {
-      lat: 53.4358,
-      lng: 2.9576
-    }
-  },
-  restaurant: {
-    name: 'The Grill on New York Street',
-    location: {
-      lat: 53.2922,
-      lng: 2.14407
-    }
-  },
-  creator: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
-
-}, {
-  event: {
-    name: 'Gentlemen\'s Dub Club',
-    date: 'Friday 19th October 2018',
-    location: {
-      lat: 51.4602,
-      lng: 0.1169
-    }
-  },
-  bar: {
-    name: String,
-    location: {
-      lat: Number,
-      lng: Number
-    }
-  },
-  restaurant: {
-    name: String,
-    location: {
-      lat: Number,
-      lng: Number
-    }
-  },
-  creator: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
-}]);
