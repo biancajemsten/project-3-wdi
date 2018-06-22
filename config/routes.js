4bf167a6847756c8334703d6c79374e4d5519598
Bianca Jemsten <biancajemsten@gmail.com>
2018-06-22 15:12:00 +0100

    added password confirmation in User model and tested register route in insomnia

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const skiddle = require('../controllers/skiddle');
-const auth = require('..controllers/auth');
+const auth = require('../controllers/auth');
 
 
 router.get('/events', skiddle.eventFinder);
 
-router.post('/register', auth.register); 
+router.post('/register', auth.register);
 
 module.exports = router;
