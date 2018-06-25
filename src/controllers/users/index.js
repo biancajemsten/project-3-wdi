ab3653701a56dab729f0f345a6cabdd784c94122
Bella Dear <arabelladear@hotmail.co.uk>
2018-06-25 16:20:48 +0100

    updated default images

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -5,6 +5,12 @@ function UsersIndexCtrl($scope, $http) {
     url: '/api/users'
   })
     .then(res => $scope.users = res.data);
+
+  $http({
+    method: 'GET',
+    url: '/api/bundles'
+  })
+    .then(res => $scope.bundles = res.data);
 }
 
 export default UsersIndexCtrl;
