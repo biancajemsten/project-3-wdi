f9685e907d7dd1291d781702013ca076f6601732
Bianca Jemsten <biancajemsten@gmail.com>
2018-06-26 12:05:45 +0100

    added front end display of adding attendees

--- src/controllers/bundles/show.js
+++ src/controllers/bundles/show.js
@@ -2,9 +2,17 @@ function BundlesShowCtrl( $scope, $http, $state){
 
   $http({
     method: 'GET',
-    url: `api/bundles/${$state.params.id}`
+    url: `/api/bundles/${$state.params.id}`
   })
     .then(res => $scope.bundle = res.data);
+
+
+  $http({
+    method: 'GET',
+    url: '/api/users'
+  })
+    .then(res => $scope.users = res.data);
+
 
   $scope.deleteBundle = function(){
     $http({
@@ -13,7 +21,25 @@ function BundlesShowCtrl( $scope, $http, $state){
     })
       .then(() => $state.go('usersShow', { id: $scope.currentUserId }));
   };
+//
+//   $scope.addAttendee = function(user){
+//     const data = {
+//       attendees: {
+//         _id: user._id,
+//         firstName: user.firstName,
+//         lastName: user.lastName
+//       }
+//     };
+//     $http({
+//       method: 'PUT',
+//       url: `/api/bundles${$state.params.id}`,
+//       data: data
+//     })
+//       .then(res => $scope.bundle = res.data);
+//     console.log(user._id);
+//   };
 }
+
 
 
 export default BundlesShowCtrl;
