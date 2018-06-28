c3b9bb2a5071ea93836413e39e72dbec29328fbe
Bianca Jemsten <biancajemsten@gmail.com>
2018-06-28 17:18:04 +0100

    set up mongoDB for deployment

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev';
 // const googleApiKey = process.env.GOOGLE_MAPS_API_KEY;
 // const skiddleApiKey = process.env.SKIDDLE_API_KEY;
-const dbURI = `mongodb://localhost/bundle-api-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/bundle-api-${env}`;
 const secret = '3_u@*BT_a@E#mXw';
 
 // console.log('skiddle', process.env.GOOGLE_MAPS_API_KEY);
